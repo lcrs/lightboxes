b017f7b458e10ed9307ba0f5f38254b474ea83b8
Lewis Saunders <lewis.saunders@gmail.com>
2014-12-26 20:55:45 +0000

    wild stuff happening. diffuse matches houdini now

--- disney_ibl.glsl
+++ disney_ibl.glsl
@@ -8,111 +8,70 @@
 
 uniform vec3 adskUID_baseColor;
 uniform float adskUID_metallic;
+uniform float adskUID_roughness;
 uniform float adskUID_subsurface;
 uniform float adskUID_specular;
-uniform float adskUID_roughness;
 uniform float adskUID_specularTint;
 uniform float adskUID_anisotropic;
 uniform float adskUID_sheen;
 uniform float adskUID_sheenTint;
 uniform float adskUID_clearcoat;
 uniform float adskUID_clearcoatGloss;
-uniform int adskUID_minsamples;
-uniform int adskUID_maxsamples;
-uniform float adskUID_variancelimit;
-uniform bool adskUID_outputvariance;
-uniform bool adskUID_outputsamples;
+uniform int adskUID_samples;
 uniform int adskUID_method;
 uniform float adskUID_lod;
 const float adskUID_PI = 3.14159265358979323846;
 
-float adskUID_sqr(float x) { return x*x; }
+float adskUID_sqr(float v) {
+    return v*v;
+}
 
-float adskUID_SchlickFresnel(float u)
-{
+float adskUID_schlick_f(float u) {
     float m = clamp(1.0-u, 0.0, 1.0);
     float m2 = m*m;
-    return m2*m2*m; // pow(m,5)
+    return m2*m2*m; 
 }
 
-float adskUID_GTR1(float NdotH, float a)
-{
-    if (a >= 1.0) return 1.0/adskUID_PI;
-    float a2 = a*a;
-    float t = 1.0 + (a2-1.0)*NdotH*NdotH;
-    return (a2-1.0) / (adskUID_PI*log(a2)*t);
-}
-
-float adskUID_GTR2(float NdotH, float a)
-{
-    float a2 = a*a;
-    float t = 1.0 + (a2-1.0)*NdotH*NdotH;
-    return a2 / (adskUID_PI * t*t);
-}
-
-float adskUID_GTR2_aniso(float NdotH, float HdotX, float HdotY, float ax, float ay)
-{
-    return 1.0 / ( adskUID_PI * ax*ay * adskUID_sqr( adskUID_sqr(HdotX/ax) + adskUID_sqr(HdotY/ay) + NdotH*NdotH ));
-}
-
-float adskUID_smithG_GGX(float Ndotv, float alphaG)
-{
+float adskUID_smith_g(float Ndotv, float alphaG) {
+    if (Ndotv <= 0.0) {
+        return 0.0;
+    }
     float a = alphaG*alphaG;
     float b = Ndotv*Ndotv;
     return 1.0/(Ndotv + sqrt(a + b - a*b));
 }
 
-vec3 adskUID_BRDF( vec3 L, vec3 V, vec3 N, vec3 X, vec3 Y )
-{
-    float NdotL = dot(N,L);
-    float NdotV = dot(N,V);
-    if (NdotL < 0.0 || NdotV < 0.0) return vec3(0.0);
+vec3 adskUID_diffuse(vec3 u, vec3 v, vec3 n) {
+    vec3 nn = normalize(n);
+    vec3 un = normalize(v);
+    vec3 vn = normalize(u);
+    vec3 h = normalize(un+vn);
+    float udoth = dot(un,h);
+    float Ndotu = dot(un,nn);
+    float a_udoth = abs(udoth);
+    float a_Ndotu = abs(Ndotu);
+    float Ndotv = dot(vn,nn);
+    if(Ndotv > 0.0) {
+        float tmp = udoth*udoth*adskUID_roughness;
 
-    vec3 H = normalize(L+V);
-    float NdotH = dot(N,H);
-    float LdotH = dot(L,H);
+        float Fd90 = 0.5 + 2.0*tmp;
+        float FV = adskUID_schlick_f(a_Ndotu);
+        float FL = adskUID_schlick_f(Ndotv);
+        float Fd = mix(1.0, Fd90, FL) * mix(1.0, Fd90, FV);
 
-    vec3 Cdlin = adskUID_baseColor;
-    float Cdlum = 0.3*Cdlin[0] + 0.6*Cdlin[1]  + 0.1*Cdlin[2]; // luminance approx.
+        float Fss90 = tmp;
+        float Fss = mix(1.0, Fss90, FL) * mix(1.0, Fss90, FV);
+        float ss = 1.25 * (Fss * (1.0/(a_Ndotu+Ndotv)-0.5) + 0.5);
 
-    vec3 Ctint = Cdlum > 0.0 ? Cdlin/Cdlum : vec3(1.0); // normalize lum. to isolate hue+sat
-    vec3 Cspec0 = mix(adskUID_specular*.08*mix(vec3(1.0), Ctint, adskUID_specularTint), Cdlin, adskUID_metallic);
-    vec3 Csheen = mix(vec3(1.0), Ctint, adskUID_sheenTint);
+        float FH = adskUID_schlick_f(a_udoth);
+        float Fsheen = FH * adskUID_sheen;
 
-    // Diffuse fresnel - go from 1 at normal incidence to .5 at grazing
-    // and mix in diffuse retro-reflection based on roughness
-    float FL = adskUID_SchlickFresnel(NdotL), FV = adskUID_SchlickFresnel(NdotV);
-    float Fd90 = 0.5 + 2.0 * LdotH*LdotH * adskUID_roughness;
-    float Fd = mix(1.0, Fd90, FL) * mix(1.0, Fd90, FV);
-
-    // Based on Hanrahan-Krueger brdf approximation of isotropic bssrdf
-    // 1.25 scale is used to (roughly) preserve albedo
-    // Fss90 used to "flatten" retroreflection based on roughness
-    float Fss90 = LdotH*LdotH*adskUID_roughness;
-    float Fss = mix(1.0, Fss90, FL) * mix(1.0, Fss90, FV);
-    float ss = 1.25 * (Fss * (1.0 / (NdotL + NdotV) - 0.5) + 0.5);
-
-    // specular
-    float aspect = sqrt(1.0-adskUID_anisotropic*0.9);
-    float ax = max(0.001, adskUID_sqr(adskUID_roughness)/aspect);
-    float ay = max(0.001, adskUID_sqr(adskUID_roughness)*aspect);
-    float Ds = adskUID_GTR2_aniso(NdotH, dot(H, X), dot(H, Y), ax, ay);
-    float FH = adskUID_SchlickFresnel(LdotH);
-    vec3 Fs = mix(Cspec0, vec3(1.0), FH);
-    float roughg = adskUID_sqr(adskUID_roughness*0.5+0.5);
-    float Gs = adskUID_smithG_GGX(NdotL, roughg) * adskUID_smithG_GGX(NdotV, roughg);
-
-    // sheen
-    vec3 Fsheen = FH * adskUID_sheen * Csheen;
-
-    // clearcoat (ior = 1.5 -> F0 = 0.04)
-    float Dr = adskUID_GTR1(NdotH, mix(0.1,0.001,adskUID_clearcoatGloss));
-    float Fr = mix(0.04, 1.0, FH);
-    float Gr = adskUID_smithG_GGX(NdotL, 0.25) * adskUID_smithG_GGX(NdotV, 0.25);
-
-    return ((1.0/adskUID_PI) * mix(Fd, ss, adskUID_subsurface)*Cdlin + Fsheen)
-        * (1.0-adskUID_metallic)
-        + Gs*Fs*Ds + 0.25*adskUID_clearcoat*Gr*Fr*Dr;
+        vec3 eval = mix(Fd, ss, adskUID_subsurface) * adskUID_baseColor + Fsheen;
+        eval *= 2.0;
+        eval *= 1.0 - adskUID_metallic;
+        eval *= Ndotv;
+        return eval;
+    }
 }
 
 uint adskUID_hash(uint x, uint y) {
@@ -156,10 +115,6 @@ vec2 adskUID_halton(uint i) {
     return result;
 }
 
-float adskUID_luma(vec3 c) {
-    return dot(c, vec3(0.2126, 0.7152, 0.0722));
-}
-
 vec3 adskUID_hemi(vec2 uv) {
     float phi = uv.y * 2.0 * adskUID_PI;
     float costheta = 1.0 - uv.x;
@@ -189,14 +144,8 @@ vec4 adskUID_lightbox(vec4 i) {
     // Accumulator
     vec3 a = vec3(0.0);
 
-    // Max samples we will ever take
-    uint sn = uint(adskUID_maxsamples);
-
-    // How many samples we actually take to get a good low variance
-    uint bail = 0u;
-
-    // State variables for continuous variance calculation
-    float m = 0.0, s = 0.0, variance = 0.0;
+    // Samples we will take
+    uint sn = uint(adskUID_samples);
 
     for(uint i = 0u; i < sn; i++) {
         // Generate position for this sample
@@ -215,34 +164,15 @@ vec4 adskUID_lightbox(vec4 i) {
         }
 
         // Go!
-        vec3 l = world2tangent * adskUID_hemi(sample);
-        vec3 env = adsk_getAngularMapIBL(0, adskUID_cylinder(l), adskUID_lod);
-        vec3 b = env * adskUID_BRDF(l, v, n, t, b) * dot(n, l);
+        vec3 u = world2tangent * adskUID_hemi(sample);
+        vec3 env = adsk_getAngularMapIBL(0, adskUID_cylinder(u), adskUID_lod);
+        vec3 b = env * adskUID_diffuse(u, v, n);
 
         // Accumulate
         a += b;
-
-        // Update variance
-        float mk1 = m;
-        float sk1 = s;
-        float k = float(i);
-        float xk = adskUID_luma(pow(b, vec3(1.0/2.4))); // Perceptual weighting, luma of gamma'd colour
-        if(i == 0u) m = xk; else m = mk1 + (xk - mk1)/k;
-        s = sk1 + (xk - mk1) * (xk - m);
-        variance = s/(k-1.0);
-        bail++;
-        if((adskUID_method != 0) && (bail >= uint(adskUID_minsamples)) && (variance < (adskUID_variancelimit/100.0))) break;
     }
 
-    vec3 thislight = a/float(bail);
+    vec3 thislight = a/float(sn);
     i.rgb += adsk_getComputedDiffuse() * i.a * adsk_getLightColour() * thislight;
-
-    if(adskUID_outputsamples) {
-        if(bail == uint(adskUID_minsamples)) i.r = 1.0; else i.r = 0.0;
-        if(bail == uint(adskUID_maxsamples)) i.b = 1.0; else i.b = 0.0;
-        i.g = float(bail)/100.0;
-    }
-    if(adskUID_outputvariance) i.rgb = vec3(variance);
-
 	return i;
 }
